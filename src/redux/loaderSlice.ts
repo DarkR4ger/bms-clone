143a05a00068f1d5549c106e81216c3c821f13eb
DarkR4ger <saravanangowtham15@gmail.com>
2024-01-04 14:33:13 +0530

    feat(*): created redux store and jswtoken

---
+++ src/redux/loaderSlice.ts
@@ -0,0 +1,16 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const loaderSlice = createSlice({
+  name: "loader",
+  initialState: {
+    loading: false,
+  },
+  reducers: {
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+  },
+});
+
+export const { setLoading } = loaderSlice.actions;
+export default loaderSlice.reducer;
